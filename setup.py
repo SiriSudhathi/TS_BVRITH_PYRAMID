911e0d4527f038e47fb6053730a234a4f2dc90db
Siri Sudhathi <sirivsudhathi@gmail.com>
2017-01-10 20:57:55 +0530

    trying mongodb with pyramid

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+requires = ['pyramid', 'pyramid_debugtoolbar', 'pymongo', 'uwsgi', 'waitress']
+
+setup(name='Proj',
+      version='0.0',
+      description='Proj',
+      long_description=README,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author="Niall O'Higgins",
+      author_email='nialljohiggins@gmail.com',
+      url='https://github.com/niallo/pyramid_mongodb',
+      keywords='web pyramid pylons mongodb',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="proj",
+      entry_points = """\
+      [paste.app_factory]
+      main = proj:main
+      """,
+      paster_plugins=['pyramid'],
+      )
